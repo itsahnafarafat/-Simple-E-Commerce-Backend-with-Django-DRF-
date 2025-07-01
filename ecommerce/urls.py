70b6ffe3545934e914ca4961ae832043584f63bf
Ahnaf Arafat <ahnafarafat.py@gmail.com>
2025-07-01 01:08:12 +0000

    📄 Add Swagger UI to project URLs

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,8 +17,26 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Swagger schema configuration
+schema_view = get_schema_view(
+    openapi.Info(
+        title="E-Commerce API",
+        default_version='v1',
+        description="Simple E-Commerce Backend with Django DRF"
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+
+    # Swagger UI
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
